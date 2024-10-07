2733cfb74518c0497cd113eead628605cef740f1
samuelcoder1 <echeverrysamuel74@mail.com>
2024-10-07 17:17:17 -0500

    feature: creating the database and the env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonController } from './common/common.controller';
-import { ConfigController } from './config/config.controller';
+import { ConfigController } from './common/config/config.controller';
 import { ServicesController } from './services/services.controller';
 import { ServicesModule } from './services/services.module';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from './common/config/config.module';
 import { CommonModule } from './common/common.module';
 
 @Module({
